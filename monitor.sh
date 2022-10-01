8719526adb04d679e57ae5ce38eab56e004b381e
Aby <aby.pzcrazy@gmail.com>
2022-10-01 16:02:16 +0900

    Cosmetic changes and reorganizing of static vars

--- monitor.sh
+++ monitor.sh
@@ -9,6 +9,7 @@ SLEEP=5
 ALIVECHECK=300
 ALIVE=0
 RUNNING='running'
+UNLOCK="python3 unlockdoor.py '$MAIN_DOOR'"
 
 echo "$$" > "$PIDFILE"
 echo `date` ": Started Monitoring" >> "$LOGFILE"
@@ -23,7 +24,7 @@ do
 		if [ `echo "$CURRENT" | wc -w ` -gt "$CURRENTEXISTS" ] 
 		then
 			echo `date` ": New Authenticated device $DEVICE connected, attempting to unlock the door" >> "$LOGFILE"
-			python3 unlockdoor.py \'$MAIN_DOOR\'
+			eval "$UNLOCK"
 			ALIVE=`expr $ALIVE + $SLEEP`
 			echo `date` ": Unlocked door" >> "$LOGFILE"
 		elif [ $ALIVE -gt $ALIVECHECK ]
@@ -34,19 +35,27 @@ do
 	done
 	CMD="arp | grep C | egrep '$APPROVED' | awk '{print \$3}' > $TESTFILE"
 	eval "$CMD"
-	ALIVE=`expr $ALIVE + $SLEEP`
-	sleep $SLEEP
 	while [ ! -f $TESTFILE ]
 	do
-		sleep 1
+		sleep $SLEEP
+		ALIVE=`expr $ALIVE + $SLEEP`
+		if [ $ALIVE -gt $ALIVECHECK ]
+		then
+			echo `date` ": Waiting for temp file.." >> "$LOGFILE"
+			ALIVE=0
+		fi
 	done
 	UPDATEDFLAG=`diff $TESTFILE $CONNECTED | egrep '<|>' | wc -l`
+	ALIVE=`expr $ALIVE + $SLEEP`
+	sleep $SLEEP
 	if [ $UPDATEDFLAG -gt 0 ]
 	then
-		`diff $TESTFILE $CONNECTED`
-		mv $TESTFILE $CONNECTED
+		#`diff $TESTFILE $CONNECTED`
+		cat $TESTFILE > $CONNECTED
 		echo `date` ": Updated current connections" >> "$LOGFILE"
 		sleep $SLEEP
+	else
+		rm $TESTFILE
 	fi
 done
 echo `date` ": Finished running" >> "$LOGFILE"
